31dc47724f81d0619dbcef40f83dbe544fd76706
javrobs <javieroblesamar@gmail.com>
2023-07-25 10:30:46 +0800

    restored manually

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from flask import render_template,jsonify
 def test(input="data_release_cycle_table"):
     dict={"data_release_cycle_table":model.data_release_cycle_table,
           "gender_table":model.gender_table}
-    result=dict[input].query.first().select.first().marital_status
+    result=dict[input].query.all()
     print(result)
     return render_template("index.html",result=result)
 
