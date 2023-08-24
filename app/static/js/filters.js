85716bfb993ed1c3389e94a7412e71b7ce088361
Daniel Murillo <danielmgiant@gmail.com>
2023-08-23 20:45:51 -0600

    Edited filter and filterDeeper JS functions

--- app/static/js/filters.js
+++ app/static/js/filters.js
@@ -3,8 +3,10 @@ const instructions=document.querySelector("#instructions");
 const plotarea=document.querySelector("#plot");
 const year2007=document.querySelector("#first-year");
 const year2017=document.querySelector("#second-year");
-const filterElement=document.querySelector("#first-filter");
+const filterElement=document.querySelector("#filter-1");
 const filterDeeperDiv=document.querySelector("#filter-deeper");
+const errorMessage=document.querySelector("#error-message")
+var filterCount=0
 
 year2007.addEventListener("change",filter);
 year2017.addEventListener("change",filter);
@@ -27,6 +29,7 @@ function filter(element){
         plotarea.classList.add("d-none");
         missingValueDiv.innerHTML="";
         filterDeeperDiv.innerHTML="";
+        errorMessage.classList.add("d-none");
     } else {
         instructions.classList.add("d-none");
         plotarea.classList.remove("d-none");
@@ -39,16 +42,28 @@ function filter(element){
                 },
             body: JSON.stringify({column: value,previousFilters:filtersDictionary})
             }).then(data=>data.json()).then(data => {
-                // console.log(data);
-                plot(data,year2007.checked);
-                filterDeeper(data,value);
+                console.log(data);
+                if(data.all_data.length!==0){
+                    plot(data,year2007.checked);
+                    errorMessage.classList.add("d-none");
+                    filterDeeper(data,value);
+                } else {
+                    plotarea.classList.add("d-none");
+                    errorMessage.classList.remove("d-none");
+                };
             });
     }
 }
 
 function filterDeeper(data,column){
     let selectedFilter=document.querySelector(`option[value=${column}]`).innerHTML;
-    filterDeeperDiv.innerHTML=`<h5 class="pt-3 pb-2 m-0">Filter deeper on <span class="highlight">${selectedFilter}</span>:</h5>`;
+    // filterDeeperDiv.innerHTML+=`<h5 class="pt-3 pb-2 m-0">Filter deeper on <span class="highlight">${selectedFilter}</span>:</h5>`;
+    let h5Title = document.createElement("h5");
+    h5Title.innerHTML=`Filter deeper on <span class="highlight">${selectedFilter}</span>:`
+    h5Title.classList.add("pt-3");
+    h5Title.classList.add("pb-2");
+    h5Title.classList.add("m-0");
+    filterDeeperDiv.appendChild(h5Title);
     let options=[];
     let newSelect=document.createElement("select");
     let defaultOption=document.createElement("option");
@@ -56,12 +71,12 @@ function filterDeeper(data,column){
     newSelect.appendChild(defaultOption);
     data.all_data.forEach(line=>{
         // console.log(options);
-        let value=line["description"];
-        value=value.replaceAll("~"," ");
-        if (options.includes(value)===false&&value!=="Don't know"&&value!=="Refused"){
-            options.push(value);
+        let text=line["description"];
+        text=text.replaceAll("~"," ");
+        if (options.includes(text)===false&&text!=="Don't know"&&text!=="Refused"){
+            options.push(text);
             let oneOption=document.createElement("option");
-            oneOption.innerHTML=value;
+            oneOption.innerHTML=text;
             oneOption.setAttribute("value",line["id"])
             newSelect.appendChild(oneOption);
         } 
@@ -81,6 +96,7 @@ function createFilter(event,column){
     });
     let newFilter=filterElement.cloneNode(true);
     newFilter.classList.remove("filter-past");
+    newFilter.removeAttribute("id");
     let optionToRemove=newFilter.querySelector(`[value=${column}]`);
     newFilter.removeChild(optionToRemove);
     filterDeeperDiv.appendChild(newFilter);
